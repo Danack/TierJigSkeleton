58fbdab38ae5b9f76a2aec8fc8e85e255893aaa9
Danack <Danack@basereality.com>
2016-01-26 22:53:52 +0000

    Add missing config.

--- src/appEnv.php
+++ src/appEnv.php
@@ -1,10 +1,13 @@
 <?php
 
+
+// This is a place-holder file
 function getAppEnv()
 {
     static $env = [
         'jig_compilecheck' => 'COMPILE_CHECK_MTIME',
         'caching_setting' => 'caching.revalidate',
+        'script_packing' => false,
     ];
 
     return $env;
